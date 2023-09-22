b466dc12f00313435cef7bce274deca3e67e8943
RostomViridis <rostom.mathlouthi@viridis-it.com>
2023-09-22 18:57:34 +0100

    lang

--- src/i18n.js
+++ src/i18n.js
@@ -9,8 +9,6 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     fallbackLng: "en",
-    supportedLngs: ["en", "fr", "de"],
-
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
     },
